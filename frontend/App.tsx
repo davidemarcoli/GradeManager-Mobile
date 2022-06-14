da55b1bb95b00802d05acac3f95e6accf3fec3d6
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-06-14 21:49:23 +0200

    install packages for input fields and implement password input field.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
 import IconButton from "./components/atoms/IconButton";
-import {View} from "react-native";
+import { View } from "react-native";
 import TextField from "./components/atoms/TextField";
+import TextInputField from "./components/atoms/TextInputField";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,14 +15,33 @@ export default function App() {
     return null;
   } else {
     return (
-        <><View style={{width: '100%', height: '100%', alignContent: "center", justifyContent: "center"}}>
-            <View style={{width: '50%', height: '15%', alignSelf: "center"}}>
-                <IconButton onPress={() => console.log("clicked")} backgroundColor={"#6C63FF"}
-                            text={{text: "Login", color: "white", size: 20, weight: "bold"}}
-                            borderRadius={10} border={{color: "grey", width: 1}}
-                            icon={{name: "google", size: "extraLarge", padding: 10, color: "white"}}/>
-            </View>
-        </View></>
+      <>
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            alignContent: "center",
+            justifyContent: "center",
+          }}
+        >
+          <View style={{ width: "50%", height: "15%", alignSelf: "center" }}>
+            <IconButton
+              onPress={() => console.log("clicked")}
+              backgroundColor={"#6C63FF"}
+              text={{ text: "Login", color: "white", size: 20, weight: "bold" }}
+              borderRadius={10}
+              border={{ color: "grey", width: 1 }}
+              icon={{
+                name: "google",
+                size: "extraLarge",
+                padding: 10,
+                color: "white",
+              }}
+            />
+          </View>
+          <TextInputField />
+        </View>
+      </>
     );
   }
 }
