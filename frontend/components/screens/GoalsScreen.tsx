fa1b7578b2159eeaa2c4f8f471e5a696b7f9cf7b
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-06-19 15:50:43 +0200

    Make Password input field a molecule component.
    Reason for type molecule is that the password field specifically has the content hiding option using a hook which is triggered by a pressable icon.

--- frontend/components/screens/GoalsScreen.tsx
+++ frontend/components/screens/GoalsScreen.tsx
@@ -3,34 +3,15 @@ import React from "react";
 import { View, Pressable } from "react-native";
 import { TextInput } from "react-native-paper";
 import { useTogglePasswordVisibility } from "../../hooks/useTogglePasswordVisibility";
+import PasswordInputField from "../molecules/PasswordInputField";
 import TextField from "../atoms/TextField";
 import TextInputField from "../atoms/TextInputField";
 
 export default function GoalsScreen() {
-  const { passwordVisibility, rightIcon, handlePasswordVisibility } =
-    useTogglePasswordVisibility();
-  const [data, setData] = React.useState({
-    username: "",
-    password: "",
-  });
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
       <TextField text={"Goals"}></TextField>
-      <TextInputField
-        label={"Password"}
-        secureTextEntry={passwordVisibility}
-        defaultValue={data.password}
-        onChangeText={(value: string) =>
-          setData({
-            ...data,
-            password: value,
-          })
-        }
-        marginTop={20}
-      ></TextInputField>
-      <Pressable onPress={handlePasswordVisibility}>
-        <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
-      </Pressable>
+      <PasswordInputField />
     </View>
   );
 }
