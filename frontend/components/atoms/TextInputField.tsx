f314917399bb028fe2228fb2047a95d90ce3c020
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-06-19 15:44:32 +0200

    Create new hook for passwort visibility.
    Click on eye icon to show / hide the text input field value.

--- frontend/components/atoms/TextInputField.tsx
+++ frontend/components/atoms/TextInputField.tsx
@@ -1,41 +1,50 @@
-import React, {useState, useEffect} from "react";
-import {View, Text, StyleSheet} from "react-native";
-import {TextInput, useTheme} from 'react-native-paper';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { TextInput, useTheme } from "react-native-paper";
 
 type TextInputFieldProps = {
-    label: string;
-    defaultValue?: string;
-    placeholder?: string;
-    secureTextEntry?: boolean;
-    marginTop?: number;
-    onChangeText?: (text: string) => void;
+  label: string;
+  defaultValue?: string;
+  placeholder?: string;
+  secureTextEntry?: boolean;
+  marginTop?: number;
+  onChangeText?: (text: string) => void;
 };
 
 export default function TextInputField({
-                                           label,
-                                           defaultValue = "",
-                                           placeholder = "",
-                                           secureTextEntry = false,
-                                           marginTop = 0,
-                                           onChangeText = () => {
-                                           },
-                                       }: TextInputFieldProps) {
+  label,
+  defaultValue = "",
+  placeholder = "",
+  secureTextEntry = false,
+  marginTop = 0,
+  onChangeText = () => {},
+}: TextInputFieldProps) {
+  const theme = useTheme();
+  const [cont, setCont] = useState("");
+  const [show, setShow] = useState(false);
+  const [rightIcon, setRightIcon] = useState("eye");
 
-    const theme = useTheme();
-
-    return (
-        <TextInput style={createStyles(marginTop).textInput} label={label} value={defaultValue} onChangeText={onChangeText} placeholder={placeholder}
-                   secureTextEntry={secureTextEntry} selectionColor={theme.colors.accent}
-                   underlineColor={theme.colors.accent} activeOutlineColor={theme.colors.accent}
-                   activeUnderlineColor={theme.colors.accent}></TextInput>
-    );
+  return (
+    <View>
+      <TextInput
+        style={createStyles(marginTop).textInput}
+        label={label}
+        value={defaultValue}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        secureTextEntry={secureTextEntry}
+        selectionColor={theme.colors.accent}
+        underlineColor={theme.colors.accent}
+        activeOutlineColor={theme.colors.accent}
+        activeUnderlineColor={theme.colors.accent}
+      ></TextInput>
+    </View>
+  );
 }
 
-const createStyles = (
-    marginTop?: number
-) =>
-    StyleSheet.create({
-        textInput: {
-            marginTop: marginTop,
-        },
-    });
+const createStyles = (marginTop?: number) =>
+  StyleSheet.create({
+    textInput: {
+      marginTop: marginTop,
+    },
+  });
--- frontend/components/screens/GoalsScreen.tsx
+++ frontend/components/screens/GoalsScreen.tsx
@@ -1,13 +1,36 @@
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import React from "react";
-import {View} from "react-native";
+import { View, Pressable } from "react-native";
+import { TextInput } from "react-native-paper";
+import { useTogglePasswordVisibility } from "../../hooks/useTogglePasswordVisibility";
 import TextField from "../atoms/TextField";
+import TextInputField from "../atoms/TextInputField";
 
 export default function GoalsScreen() {
-
-    return (
-        <View style={{flex: 1, justifyContent: "center"}}>
-            <TextField text={"Goals"}></TextField>
-        </View>
-    )
-
+  const { passwordVisibility, rightIcon, handlePasswordVisibility } =
+    useTogglePasswordVisibility();
+  const [data, setData] = React.useState({
+    username: "",
+    password: "",
+  });
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <TextField text={"Goals"}></TextField>
+      <TextInputField
+        label={"Password"}
+        secureTextEntry={passwordVisibility}
+        defaultValue={data.password}
+        onChangeText={(value: string) =>
+          setData({
+            ...data,
+            password: value,
+          })
+        }
+        marginTop={20}
+      ></TextInputField>
+      <Pressable onPress={handlePasswordVisibility}>
+        <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
+      </Pressable>
+    </View>
+  );
 }
