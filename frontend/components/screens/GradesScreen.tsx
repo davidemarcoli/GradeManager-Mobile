871d382ae9c52c3a8e7aa03d97c48adc9ee2c389
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-06-21 19:45:54 +0200

    Add input fields for grad management page.
    Adjusted text input field and added suiting icons and keybord types (ex: grade = numeric input).

--- frontend/components/screens/GradesScreen.tsx
+++ frontend/components/screens/GradesScreen.tsx
@@ -6,8 +6,10 @@ import TextInputField from "../atoms/TextInputField";
 
 export default function GradesScreen() {
   const [data, setData] = React.useState({
-    username: "",
-    password: "",
+    grade: "",
+    gradename: "",
+    subject: "",
+    school: "",
   });
   const theme = useTheme();
 
@@ -20,36 +22,83 @@ export default function GradesScreen() {
         marginRight: 40,
       }}
     >
-      <Card>
-        <Card.Content>
-          <Title>Add new grade</Title>
-        </Card.Content>
-        <Card.Actions style={{}}>
-          <TextInputField
-            label={"Grade"}
-            defaultValue={data.username}
-            onChangeText={(value: string) =>
-              setData({
-                ...data,
-                username: value,
-              })
-            }
-            marginTop={20}
-            mainIcon={{
-              name: "google",
-              size: "medium",
-              color: theme.colors.text,
-            }}
-            subtext={{
-              text: "ex: 4.5",
-              color: theme.colors.text,
-              fontSize: 10,
-              marginTop: 10,
-            }}
-          ></TextInputField>
-          <Button>Add Grade</Button>
-        </Card.Actions>
-      </Card>
+      <TextInputField
+        label={"Grade"}
+        defaultValue={data.grade}
+        onChangeText={(value: string) =>
+          setData({
+            ...data,
+            grade: value,
+          })
+        }
+        marginTop={20}
+        mainIcon={{
+          name: "numeric",
+          size: "medium",
+          color: theme.colors.text,
+        }}
+        subtext={{
+          text: "ex: 4.5",
+        }}
+        numeric={true}
+      ></TextInputField>
+      <TextInputField
+        label={"Grade Name"}
+        defaultValue={data.gradename}
+        onChangeText={(value: string) =>
+          setData({
+            ...data,
+            gradename: value,
+          })
+        }
+        marginTop={20}
+        mainIcon={{
+          name: "file-document-outline",
+          size: "medium",
+          color: theme.colors.text,
+        }}
+        subtext={{
+          text: "ex: Trigonometry 2",
+        }}
+      ></TextInputField>
+      <TextInputField
+        label={"Subject"}
+        defaultValue={data.subject}
+        onChangeText={(value: string) =>
+          setData({
+            ...data,
+            subject: value,
+          })
+        }
+        marginTop={20}
+        mainIcon={{
+          name: "book-outline",
+          size: "medium",
+          color: theme.colors.text,
+        }}
+        subtext={{
+          text: "ex: Mathematics",
+        }}
+      ></TextInputField>
+      <TextInputField
+        label={"School"}
+        defaultValue={data.school}
+        onChangeText={(value: string) =>
+          setData({
+            ...data,
+            school: value,
+          })
+        }
+        marginTop={20}
+        mainIcon={{
+          name: "school-outline",
+          size: "medium",
+          color: theme.colors.text,
+        }}
+        subtext={{
+          text: "ex: BMS",
+        }}
+      ></TextInputField>
     </View>
   );
 }
--- frontend/components/screens/RegisterScreen.tsx
+++ frontend/components/screens/RegisterScreen.tsx
@@ -1,62 +1,62 @@
 import React from "react";
 import TextField from "../atoms/TextField";
-import {StyleSheet, View} from "react-native";
+import { StyleSheet, View } from "react-native";
 import TextInputField from "../atoms/TextInputField";
 import IconButton from "../atoms/IconButton";
-import {Text, useTheme} from "react-native-paper";
-import {useNavigation} from "@react-navigation/native";
+import { Text, useTheme } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 export default function RegisterScreen() {
-    const [data, setData] = React.useState({
-        username: "",
-        password: "",
-        fullname: "",
-    });
+  const [data, setData] = React.useState({
+    username: "",
+    password: "",
+    fullname: "",
+  });
 
-    function login() {
-        console.log(data);
-    }
+  function login() {
+    console.log(data);
+  }
 
-    function loginWithGoogle() {
-        console.log(data);
-    }
+  function loginWithGoogle() {
+    console.log(data);
+  }
 
-    const theme = useTheme();
-    const navigation = useNavigation();
+  const theme = useTheme();
+  const navigation = useNavigation();
 
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                marginLeft: 40,
-                marginRight: 40,
-            }}
-        >
-            <TextField
-                text={"Register"}
-                textSize={30}
-                fontWeight={"bold"}
-                alignments={"flex-start"}
-            ></TextField>
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        marginLeft: 40,
+        marginRight: 40,
+      }}
+    >
+      <TextField
+        text={"Register"}
+        textSize={30}
+        fontWeight={"bold"}
+        alignments={"flex-start"}
+      ></TextField>
 
-            <IconButton
-                border={{width: 1}}
-                marginTop={30}
-                height={50}
-                borderRadius={5}
-                onPress={() => {
-                    loginWithGoogle();
-                }}
-                icon={{name: "google", size: "extraLarge", color: theme.colors.text}}
-                backgroundColor={theme.colors.navbarBackground}
-            ></IconButton>
+      <IconButton
+        border={{ width: 1 }}
+        marginTop={30}
+        height={50}
+        borderRadius={5}
+        onPress={() => {
+          loginWithGoogle();
+        }}
+        icon={{ name: "google", size: "extraLarge", color: theme.colors.text }}
+        backgroundColor={theme.colors.navbarBackground}
+      ></IconButton>
 
-            <TextField
-                text={"Or, register with email.."}
-                textSize={12}
-                marginTop={30}
-            ></TextField>
+      <TextField
+        text={"Or, register with email.."}
+        textSize={12}
+        marginTop={30}
+      ></TextField>
 
       <TextInputField
         label={"Email"}
@@ -103,40 +103,40 @@ export default function RegisterScreen() {
         }}
       ></TextInputField>
 
-            <IconButton
-                marginTop={30}
-                height={50}
-                borderRadius={5}
-                onPress={() => {
-                    login();
-                }}
-                text={{text: "Register", weight: "bold"}}
-                backgroundColor={theme.colors.accent}
-            ></IconButton>
+      <IconButton
+        marginTop={30}
+        height={50}
+        borderRadius={5}
+        onPress={() => {
+          login();
+        }}
+        text={{ text: "Register", weight: "bold" }}
+        backgroundColor={theme.colors.accent}
+      ></IconButton>
 
-            <View
-                style={{
-                    flexDirection: "row",
-                    marginTop: 25,
-                    justifyContent: "center",
-                }}
-            >
-                <TextField text={"Already have an account? "} textSize={12}></TextField>
-                <Text
-                    onPress={() => {
-                        navigation.navigate("Login");
-                    }}
-                    style={{color: "#6C63FF"}}
-                >
-                    Login
-                </Text>
-            </View>
-        </View>
-    );
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 25,
+          justifyContent: "center",
+        }}
+      >
+        <TextField text={"Already have an account? "} textSize={12}></TextField>
+        <Text
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+          style={{ color: "#6C63FF" }}
+        >
+          Login
+        </Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    space: {
-        marginBottom: 10,
-    },
+  space: {
+    marginBottom: 10,
+  },
 });
