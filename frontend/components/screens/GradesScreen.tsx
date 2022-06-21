47d79d57e38d8905b37cabec706fc7a2b3588c96
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-06-21 11:35:28 +0200

    Create react native paper card for the grade options.
    Will be used to create a new grade by filling out input fields inside the card.

--- frontend/components/screens/GradesScreen.tsx
+++ frontend/components/screens/GradesScreen.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import {View} from "react-native";
+import { View } from "react-native";
 import TextField from "../atoms/TextField";
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
 export default function GradesScreen() {
-
-    return (
-        <View style={{flex: 1, justifyContent: "center"}}>
-            <TextField text={"Grades"}></TextField>
-        </View>
-    )
-
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        marginLeft: 40,
+        marginRight: 40,
+      }}
+    >
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Card>
+          <Card.Content>
+            <Title>Add new grade</Title>
+          </Card.Content>
+          <Card.Actions>
+            <Button>Add Grade</Button>
+          </Card.Actions>
+        </Card>
+      </View>
+    </View>
+  );
 }
