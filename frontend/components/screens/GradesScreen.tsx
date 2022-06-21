ff0bd94321c3967c0cca3af8d0bb5039c1c7ae5b
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-06-21 11:50:10 +0200

    added first input field: Grade

--- frontend/components/screens/GradesScreen.tsx
+++ frontend/components/screens/GradesScreen.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { View } from "react-native";
 import TextField from "../atoms/TextField";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { Avatar, Button, Card, Title, useTheme } from "react-native-paper";
+import TextInputField from "../atoms/TextInputField";
 
 export default function GradesScreen() {
+  const [data, setData] = React.useState({
+    username: "",
+    password: "",
+  });
+  const theme = useTheme();
+
   return (
     <View
       style={{
@@ -13,16 +20,36 @@ export default function GradesScreen() {
         marginRight: 40,
       }}
     >
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Card>
-          <Card.Content>
-            <Title>Add new grade</Title>
-          </Card.Content>
-          <Card.Actions>
-            <Button>Add Grade</Button>
-          </Card.Actions>
-        </Card>
-      </View>
+      <Card>
+        <Card.Content>
+          <Title>Add new grade</Title>
+        </Card.Content>
+        <Card.Actions style={{}}>
+          <TextInputField
+            label={"Grade"}
+            defaultValue={data.username}
+            onChangeText={(value: string) =>
+              setData({
+                ...data,
+                username: value,
+              })
+            }
+            marginTop={20}
+            mainIcon={{
+              name: "google",
+              size: "medium",
+              color: theme.colors.text,
+            }}
+            subtext={{
+              text: "ex: 4.5",
+              color: theme.colors.text,
+              fontSize: 10,
+              marginTop: 10,
+            }}
+          ></TextInputField>
+          <Button>Add Grade</Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 }
