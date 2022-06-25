81eac235a24fe90f0d8f42274109259986c69401
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-06-25 11:50:55 +0200

    Moved settings button.
    Improved positioning with %.

--- frontend/components/screens/ProfileScreen.tsx
+++ frontend/components/screens/ProfileScreen.tsx
@@ -1,54 +1,61 @@
 import TextField from "../atoms/TextField";
-import {StyleSheet, View} from "react-native";
+import { StyleSheet, View } from "react-native";
 import ProfileCard from "../organisms/ProfileCard";
 import React from "react";
 import IconButton from "../atoms/IconButton";
-import {logout} from "../../services/UserService";
-import {useNavigation} from "@react-navigation/native";
-import {useTheme} from "react-native-paper";
+import { logout } from "../../services/UserService";
+import { useNavigation } from "@react-navigation/native";
+import { useTheme } from "react-native-paper";
 
 type ProfileScreenProps = {
-    setIsLoggedIn: (value: boolean) => void
-}
+  setIsLoggedIn: (value: boolean) => void;
+};
 
-export default function ProfileScreen({setIsLoggedIn}: ProfileScreenProps) {
+export default function ProfileScreen({ setIsLoggedIn }: ProfileScreenProps) {
+  const navigation = useNavigation();
+  const theme = useTheme();
 
-    const navigation = useNavigation()
-    const theme = useTheme()
-
-    return (
-        <>
-            <View style={styles.settingsIcon}><IconButton onPress={() => navigation.navigate("Settings")} icon={{
-                name: "cog-outline",
-                iconType: "MaterialCommunityIcons",
-                color: theme.colors.text,
-                size: "extraLarge"
-            }} backgroundColor={theme.colors.background}></IconButton></View>
-            <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-                <View style={{width: "80%", height: "30%", paddingTop: 20}}>
-                    <ProfileCard/>
-                </View>
-                <View style={{width: "80%", height: "30%", paddingTop: 20}}>
-                    <IconButton
-                        onPress={async () => {
-                            await logout()
-                            navigation.navigate("Login")
-                            setIsLoggedIn(false)
-
-                        }
-                        } text={{text: "LOGOUT", size: 20, weight: "bold"}} backgroundColor={"#8D0000"}
-                        borderRadius={10}
-                        border={{color: "black", width: 1}} height={50}></IconButton>
-                </View>
-            </View>
-        </>
-    )
+  return (
+    <>
+      <View style={styles.settingsIcon}>
+        <IconButton
+          onPress={() => navigation.navigate("Settings")}
+          icon={{
+            name: "cog-outline",
+            iconType: "MaterialCommunityIcons",
+            color: theme.colors.text,
+            size: "extraLarge",
+          }}
+          backgroundColor={theme.colors.background}
+        ></IconButton>
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View style={{ width: "80%", height: "30%", paddingTop: 20 }}>
+          <ProfileCard />
+        </View>
+        <View style={{ width: "80%", height: "30%", paddingTop: 20 }}>
+          <IconButton
+            onPress={async () => {
+              await logout();
+              navigation.navigate("Login");
+              setIsLoggedIn(false);
+            }}
+            text={{ text: "LOGOUT", size: 20, weight: "bold" }}
+            backgroundColor={"#8D0000"}
+            borderRadius={10}
+            border={{ color: "black", width: 1 }}
+            height={50}
+          ></IconButton>
+        </View>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    settingsIcon: {
-        position: "absolute",
-        top: 40,
-        right: 20,
-    }
-})
+  settingsIcon: {
+    position: "absolute",
+    top: 40,
+    right: "10%",
+  },
+});
